d7dbf0ee7dbe9f3251e504644e3a74cadfbcfb48
phirmware <chibuzor.ojukwu@gmail.com>
2019-11-03 11:10:49 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(private router: Router) {}
   title = 'codeclub-members';
+  routerPath: any = '/members';
+
+  setRouterPath(e) {
+    const sideNavs = document.querySelectorAll('.sidenav');
+    for (let i = 0; i < sideNavs.length; i++) {
+      sideNavs[i].classList.remove('mm-active');
+    }
+    e.target.classList.add('mm-active');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { MembersComponent } from './members/members.component';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'members', pathMatch: 'full' },
+  { component: MembersComponent, path: 'members' },
+  { component: SignupComponent, path: 'signup' },
+  { component: LoginComponent, path: 'login' },
+  { component: DashboardComponent, path: 'dashboard' },
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, MembersComponent, SignupComponent, LoginComponent, DashboardComponent],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
